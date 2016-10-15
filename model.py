116695d11998c124d2915fe09090e144daf158ba
Thomas McColgan <thomas.mccolgan@gmail.com>
2016-10-15 12:26:28 +0200

    old changes

--- model.py
+++ model.py
@@ -61,9 +61,9 @@ bracket = pd.read_csv("TourneySlots.csv")
 mm_teams = pd.read_csv("TourneySeeds.csv")
 seeds = dict(zip(mm_teams[mm_teams['Season']==2016]['Seed'],mm_teams[mm_teams['Season']==2016]['Team']))
 mm_teams = mm_teams[mm_teams['Season']==2016]['Team'].unique()
-observed_matches = pd.read_csv('Prelim_RegularSeasonCompactResults_thru_Day132.csv')
-observed_matches = observed_matches[observed_matches['wteam'].isin(mm_teams) | observed_matches['lteam'].isin(mm_teams)].reset_index()
-teams = list(set(np.hstack((observed_matches['lteam'].unique() , observed_matches['wteam'].unique()))))
+#observed_matches = pd.read_csv('Prelim_RegularSeasonCompactResults_thru_Day132.csv')
+#observed_matches = observed_matches[observed_matches['wteam'].isin(mm_teams) | observed_matches['lteam'].isin(mm_teams)][-640:].reset_index()
+teams = mm_teams #list(set(np.hstack((observed_matches['lteam'].unique() , observed_matches['wteam'].unique()))))
 
 N = len(teams)
 team_to_ind = dict(zip(teams,range(N)))
@@ -78,30 +78,36 @@ for i in range(N):
     pace[i] = pymc.Exponential('pace_%i' % i,0.1)
 
 #observed data from games before the tournament
-N_obs = observed_matches.shape[0]
-observed_score = np.empty(N_obs, dtype=object)
-
-for n,match in observed_matches.iterrows():
-    fact = 1.+0.125*match.numot
-    if match.wloc == 'A':
-        hteam = team_to_ind[match.lteam]
-        ateam = team_to_ind[match.wteam]
-        hscore = int(match.lscore/fact)
-        ascore = int(match.wscore/fact)
-    else:
-        ateam = team_to_ind[match.lteam]
-        hteam = team_to_ind[match.wteam]
-        ascore = int(match.lscore/fact)
-        hscore = int(match.wscore/fact)
-    observed_score[n] = BivariatePoisson('observed_score_%i' % n,
-            l_1 = attack_strength[hteam]-defense_strength[ateam],
-            l_2 = attack_strength[ateam]-defense_strength[hteam],
-            l_3 = pace[hteam] + pace[ateam], observed = True, value = [hscore,ascore])
-
+#N_obs = observed_matches.shape[0]
+#observed_score = np.empty(N_obs, dtype=object)
+#
+#for n,match in observed_matches.iterrows():
+#    fact = 1.+0.125*match.numot
+#    if match.wloc == 'A':
+#        hteam = team_to_ind[match.lteam]
+#        ateam = team_to_ind[match.wteam]
+#        hscore = int(match.lscore/fact)
+#        ascore = int(match.wscore/fact)
+#    else:
+#        ateam = team_to_ind[match.lteam]
+#        hteam = team_to_ind[match.wteam]
+#        ascore = int(match.lscore/fact)
+#        hscore = int(match.wscore/fact)
+#    observed_score[n] = BivariatePoisson('observed_score_%i' % n,
+#            l_1 = attack_strength[hteam]-defense_strength[ateam],
+#            l_2 = attack_strength[ateam]-defense_strength[hteam],
+#            l_3 = pace[hteam] + pace[ateam], observed = True, value = [hscore,ascore])
+opn = pd.read_csv('open_odds.csv')
+win = pd.read_csv('win_odds.csv')
+id_to_name = dict(zip(win['id'],win.name))
+opn_dict = dict(zip(zip(opn.t_home,opn.t_away),(1./opn.o_home+1./opn.o_away)/opn.o_home))
+opn_dict.update(dict(zip(zip(opn.t_away,opn.t_home),(1./opn.o_home+1./opn.o_away)/opn.o_away)))
+win_dict = dict(zip(win.id,np.log(1/win.odds)))
 predicted_score = np.empty(len(mm_teams)*(len(mm_teams)-1)/2, dtype=object)
 
 i = 0
 teams_to_match = dict()
+match_winner_potential = []
 for n,t1 in enumerate(mm_teams):
     for t2 in mm_teams[n+1:]:
         teams_to_match[(t1,t2)] = i
@@ -112,7 +118,18 @@ for n,t1 in enumerate(mm_teams):
             l_1 = attack_strength[hteam]-defense_strength[ateam],
             l_2 = attack_strength[ateam]-defense_strength[hteam],
             l_3 = pace[hteam] + pace[ateam])
+        if t1 in id_to_name.keys() and t2 in id_to_name.keys() and (id_to_name[t1],id_to_name[t2]) in opn_dict.keys(): 
+            print (id_to_name[t1],id_to_name[t2])
+            @pymc.potential
+            def match_win_pot(score = predicted_score[i], odds=opn_dict[(id_to_name[t1],id_to_name[t2])]):
+                if score[0]>score[1]:
+                    return np.log(odds)
+                else:
+                    return np.log(1.-odds)
+            match_winner_potential.append(match_win_pot)
         i += 1
+match_winner_potential = np.array(match_winner_potential)
+
 @pymc.deterministic(trace=True)
 def played_winner(predicted_score=predicted_score):
     played = np.zeros(predicted_score.shape[0]+1)*np.nan
@@ -125,10 +142,12 @@ def played_winner(predicted_score=predicted_score):
         else:
             winner[m.Slot] = winner[m.Weakseed]
             played[match_ind] = 0.
-    played[-1] = team_to_ind[winner['R6CH']]
+    played[-1] = winner['R6CH']
     return played
 
-    
+@pymc.potential
+def tourney_winner_pot(played_winner=played_winner):
+    return win_dict[played_winner[-1]]
 
 #odds_matches = []
 #odds_winner = []
@@ -201,5 +220,5 @@ def played_winner(predicted_score=predicted_score):
 #    spread_score_potential[match] = spread_score_pot
 #
 model = pymc.MCMC(locals())
-model.sample(iter=100000, burn=5000, thin=10)
-pymc.Matplot.plot(model)
+model.sample(iter=37000, burn=1000, thin=10)
+#pymc.Matplot.plot(model)
